af79d0d487f383679ee2286ed659716463827368
johan <johan@bareksa.com>
2018-07-01 10:46:37 +0700

    asdasd

---
+++ application/views/user/posting.php
@@ -0,0 +1,51 @@
+   
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3 class="page-header"><?=$title?></h3>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">   
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div role="tabpanel">
+                                <!-- Tab panes -->
+                                <div class="tab-content">
+                                    <div role="tabpanel" class="tab-pane fade in active" id="advance">
+                                        <div class="dataTable_wrapper" style="overflow: auto;">
+
+                                        <table class="table table-striped table-bordered table-hover" id="dataTables-user-log-advance">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Postingan Forum </th>
+                                                        <th>Delete </th>
+                                                    </tr>
+                                                   </thead>
+                                                <tbody></tbody>
+                                                <tfoot></tfoot>
+                                            </table>
+                                           </div>
+
+                                           <!-- m /.table-responsive -->
+                                    </div>
+
+                                
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+
+                                        
+
+
