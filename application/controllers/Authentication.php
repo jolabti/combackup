e964919852022a3cc6d50d6a09e88a635a1d360f
johan <johan@bareksa.com>
2018-08-05 01:55:29 +0700

    update

--- application/controllers/Authentication.php
+++ application/controllers/Authentication.php
@@ -6,15 +6,15 @@ if (!defined('BASEPATH')) {
 
 class Authentication extends CI_Controller {
 
-    public function __Construct() {
-        parent::__Construct();
-        // load to authentication model
-        $this->load->model("authentication_model");
-    }
+    // public function __Construct() {
+    //     parent::__Construct();
+    //     // load to authentication model
+    //     $this->load->model("authentication_model");
+    // }
 
     // index controller login
     public function index() {
-    
+
         if($this->session->userdata('logged_in')) {
             redirect(base_url("dashboard"));
         }else {
@@ -43,19 +43,19 @@ class Authentication extends CI_Controller {
                 'role' => $validate[0]->role,
                 'user_id' => $validate[0]->user_id,
                 'logged_in' => TRUE,
-              
+
             );
             $this->session->set_userdata($newdata);
-            redirect(base_url("dashboard")); 
+            redirect(base_url("dashboard"));
         }
         else{
             $data = array('alert' => true);
             // load to view login
             $this->load->view('login',$data);
         }
-     
+
     }
-    
+
     // function controller Chage pass
     function change_password(){
         $this->ajax_checking();
