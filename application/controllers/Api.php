38dc5b090f2ae2e3035aca07d299b849bbd033f9
johan <johan@bareksa.com>
2018-08-05 01:35:53 +0700

    commit toa

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -16,11 +16,13 @@ if (!defined('BASEPATH')) {
 
 class Api extends CI_Controller {
 
+
+
  // ===============================  Aunth Api =======================================
     public function __construct() {
        parent::__construct();
 
-       $this->load->model("user_model");
+       //$this->load->model("user_model");
        header("Access-Control-Allow-Origin: *");
        header("Content-Type: application/json; charset=UTF-8");
        header("Access-Control-Allow-Methods: POST");
